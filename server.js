c20ceaf034b9188a6e7ccba0511f92f82e76a066
Pedro_Cunhaaa <pedro.cunhab@outlook.com>
2025-12-27 18:03:25 -0300

    Adicionado próximas funções para a imagem

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' })); 
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static('public'));
 
 const CAMINHO_USUARIOS = './usuarios.json';
